d71aadf0d1d86d8ba284fee55ea85ceb0a0ef948
Bryant <bryant@kodait.com>
2023-02-03 16:53:43 +1300

    add test changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,66 @@
-import { useEffect, useRef, useState } from 'react'
-import Webcam from "react-webcam";
-import { BrowserQRCodeReader } from '@zxing/browser';
-
+import {useEffect, useRef, useState} from 'react'
+import * as ZXing from '@zxing/library'
 
 const videoConstraints = {
-  facingMode: "environment"
-};
+  facingMode: 'environment',
+}
 
-  const codeReader = new BrowserQRCodeReader();
+const log = obj => alert(`${JSON.stringify(obj)}`)
+
+const codeReader = new ZXing.BrowserQRCodeReader()
 function App() {
-  const [image, setImage] = useState(() => localStorage.getItem('barcode'))
-  const [result, setResult] = useState(null)
-  const imgRef = useRef(null)
+  const [devices, setDevices] = useState([])
+  const [selectedDevice, setSelectedDevice] = useState(null)
 
+  useEffect(() => {
+    codeReader
+      .getVideoInputDevices()
+      .then(devices => {
+        log(devices)
+        return devices.map(({label, deviceId}) => ({label, value: deviceId}))
+      })
+      .then(items => {
+        setDevices(items)
+        setSelectedDevice(items[0]['value'])
+      })
+  }, [])
 
-  const onReset = () => {
-
-  }
-
-  const onScan = async (getScreenshot) => {
-    setImage(getScreenshot())
-
-javascriptBarcodeReader({
-  /* Image file Path || {data: Uint8ClampedArray, width, height} || HTML5 Canvas ImageData */
-  image: document.querySelector('#img'),
-  barcode: 'ean-13',
-  // barcodeType: 'industrial',
-  options: {    
-    // useAdaptiveThreshold: true // for images with sahded portions
-    // singlePass: true
-  }
-})
-  .then(code => {
-    alert(`success ${code}`)
-    console.log(code)
-  })
-  .catch(err => {
-    alert(`failed ${JSON.stringify(err)}`)
-    console.log(err)
-  })
-    // const el = document.querySelector('#img')
-    // const code = await codeReader.decodeFromImageElement(el)
-    // console.log(code)
+  const onStart = () => {
+    alert(selectedDevice)
+    codeReader
+      .decodeOnceFromVideoDevice(selectedDevice, 'video')
+      .then(result => {
+        alert(`result: ${result}`)
+      })
+      .catch(err => {
+        alert(`error: ${JSON.stringify(err)}`)
+      })
   }
 
   return (
     <div id="app">
-      <Webcam
-        audio={false}
-        height={500}
-        screenshotFormat="image/jpeg"
-        width="100%"
-        videoConstraints={videoConstraints}
+      <select
+        value={selectedDevice}
+        onChange={event => setSelectedDevice(event.target.value)}
       >
-        {({ getScreenshot }) => (
-          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '10px auto'}}>
+        {devices.map(({label, value}) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+      <button onClick={onStart}>Start</button>
 
-          <button onClick={() => onScan(getScreenshot)} >
-            Capture photo
-          </button>
-          <button onClick={onReset}>Reset</button>
-          </div>
-        )}
-      </Webcam>   
-    {image && 
-
-      <img id="img" src={image} alt="" />
-    }
+      <div>
+        <video
+          id="video"
+          width="600"
+          height="400"
+          style={{border: '1px solid #eee'}}
+        ></video>
+      </div>
     </div>
-  );}
+  )
+}
 
 export default App
